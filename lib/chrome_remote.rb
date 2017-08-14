e2b50981b4612faa069417da0454b13d532649d3
Luismi Cavalle <luismi@lmcavalle.com>
2017-08-14 12:32:00 +0200

    Get the first ‘page’ returned by endpoint

--- lib/chrome_remote.rb
+++ lib/chrome_remote.rb
@@ -1,8 +1,30 @@
 require "chrome_remote/version"
 require "chrome_remote/client"
+require "json"
 
 module ChromeRemote
-  def self.client(*args, &block)
-    Client.new(*args, &block)
+  class << self
+    DEFAULT_OPTIONS = {
+      host: "localhost",
+      port: 9222
+    }
+
+    def client(options = {})
+      options = DEFAULT_OPTIONS.merge(options)
+
+      Client.new(get_ws_url(options))
+    end
+
+    private
+
+    def get_ws_url(options)
+      response = Net::HTTP.get(options[:host], "/json", options[:port])
+      # TODO handle unsuccesful request
+      response = JSON.parse(response)
+
+      first_page = response.find {|e| e["type"] == "page"} 
+      # TODO handle no entry found
+      first_page["webSocketDebuggerUrl"]
+    end
   end
 end

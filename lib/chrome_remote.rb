69b5e8fa28168c9794c2b311c0298fd3ff015c42
Mikhail Pospelov <m.pospelov@me.com>
2019-05-15 19:01:46 +0300

    add ability to connect to browserless directly

--- lib/chrome_remote.rb
+++ lib/chrome_remote.rb
@@ -20,6 +20,12 @@ module ChromeRemote
 
     def get_ws_url(options)
       query = options[:token] ? "?token=#{options[:token]}" : ''
+      return options[:ws_endpoint_url] + query if options[:ws_endpoint_url]
+
+      get_from_json_response(options, query)
+    end
+
+    def get_from_json_response(options, query)
       response = Net::HTTP.get(options[:host], "/json" + query, options[:port])
       # TODO handle unsuccesful request
       response = JSON.parse(response)
